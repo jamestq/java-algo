1c09f1652abcf24005ef827f51b767f6ffab53f7
jamestq <57053746+jamestq@users.noreply.github.com>
2023-02-16 19:54:06 +1100

    add warmup algorithm problems

---
+++ warmup/CheckDuplicate.java
@@ -0,0 +1,14 @@
+package warmup;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CheckDuplicate {
+    public boolean containsDuplicate(int[] nums) { 
+        
+        return false;
+    }
+}
+
+
+
