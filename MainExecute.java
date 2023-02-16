bb08695c2444d298c3125b30790b8acd2301998c
jamestq <57053746+jamestq@users.noreply.github.com>
2023-02-16 19:51:25 +1100

    add terminal interaction

--- MainExecute.java
+++ MainExecute.java
@@ -1,11 +1,27 @@
-import twopointers.*;
+import tests.*;
+import java.util.Scanner;
 
 public class MainExecute{
+
+    private static String[] PROGRAMS = {
+        "Find a pair that matches the target sum. ",
+        "Check if an array has duplicates", 
+    };
+
+
     public static void main(String[] args){
-        TargetSum targetSum = new TargetSum(6);
-
-        int[] array = {1,2,3,4,6};
-
-        System.out.println(targetSum.getPair(array));
+        System.out.println("Select the following programs to run: ");
+        for(int i=0; i<PROGRAMS.length; i++){
+            System.out.println((i+1) + ". " + PROGRAMS[i]);
+        }
+        System.out.print("> ");
+        Scanner userInput = new Scanner(System.in);
+        int choice = Integer.parseInt(userInput.nextLine());
+        System.out.println("Running program...");
+        switch(choice){
+            case 1: Test.testPairSum(); break;
+            case 2: Test.testDuplicate(); break;
+        }
+        userInput.close();
     }
 }
